9ad249e7b15e87a064dacc696b111128a13ffaa1
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-11 18:41:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 
-
-export default function App(){
-return (
-<div className="min-h-screen flex flex-col">
-<Navbar />
-<main className="flex-1">
-<Home />
-<About />
-<Projects />
-<Contact />
-</main>
-<Footer />
-</div>
-)
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-slate-900 text-white">
+      <Navbar />
+      <main className="flex-1">
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
 }
