96d3904708571c638bb9835399e658c8d80f1be7
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-13 17:02:42 +0530

    Create storage.js

---
+++ src/utils/storage.js
@@ -0,0 +1,8 @@
+export const storage = {
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  get: (key) => {
+    const value = localStorage.getItem(key)
+    return value ? JSON.parse(value) : null
+  },
+  remove: (key) => localStorage.removeItem(key)
+}
