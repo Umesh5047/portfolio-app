a0c3cf7964ba9f53bc70237bbf3c02e5f2be60cd
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-13 17:50:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
-import Projects from './pages/Projects'
-import ProjectDetail from './pages/ProjectDetail'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import { useAuth } from './context/AuthContext'
+import ProjectDetail from './pages/ProjectDetail'
 
 function App() {
-  const { user } = useAuth()
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 container mx-auto px-4 py-6">
+      <main className="flex-1 p-6">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/portfolio"
-            element={user ? <Portfolio /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/projects"
-            element={user ? <Projects /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/projects/:id"
-            element={user ? <ProjectDetail /> : <Navigate to="/login" />}
-          />
+          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
         </Routes>
       </main>
     </div>
