dd8b3ac1f2e2944e81630628d5f3ddd5c18361f8
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 10:21:19 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
-import ContactForm from '../components/ContactForm'
-import ProjectCard from '../components/ProjectCard'
-import { projects } from '../sample-data/data'
+import React from 'react';
+import ContactForm from '../components/ContactForm';
 
-export default function Home() {
+function Home() {
   return (
-    <div className="space-y-6">
+    <div className="p-6">
       <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {projects.map((p) => <ProjectCard key={p.id} project={p} />)}
-      </div>
+      <p>This is the home page.</p>
       <ContactForm />
     </div>
-  )
+  );
 }
+
+export default Home;
