01be2adf4dcef1ed08cc6e9af29d1845f437189c
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 12:15:23 +0530

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,20 +1,20 @@
 export const save = (k, v) => {
   if (typeof window !== 'undefined') {
-    localStorage.setItem(k, JSON.stringify(v))
+    localStorage.setItem(k, JSON.stringify(v));
   }
-}
+};
 
 export const load = (k) => {
-  if (typeof window === 'undefined') return null
+  if (typeof window === 'undefined') return null;
   try {
-    return JSON.parse(localStorage.getItem(k))
+    return JSON.parse(localStorage.getItem(k));
   } catch {
-    return null
+    return null;
   }
-}
+};
 
 export const remove = (k) => {
   if (typeof window !== 'undefined') {
-    localStorage.removeItem(k)
+    localStorage.removeItem(k);
   }
-}
+};
