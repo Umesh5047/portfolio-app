d9b1ad175911d881927f5d87d0d2cd8505fbeb6f
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 11:33:34 +0530

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,5 +1,3 @@
-export const storage = {
-  get: (key) => JSON.parse(localStorage.getItem(key)),
-  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-  remove: (key) => localStorage.removeItem(key),
-};
+export const save = (k,v)=> localStorage.setItem(k, JSON.stringify(v))
+export const load = (k)=> { try{ return JSON.parse(localStorage.getItem(k)) }catch{return null} }
+export const remove = (k)=> localStorage.removeItem(k)
