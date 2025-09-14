fa0700c5b5a1523b8c87313bf81f97b1641b4725
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 12:42:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-import ThemeToggle from './ThemeToggle'
+import { useAuth } from '../context/AuthContext.jsx'
+import ThemeToggle from './ThemeToggle.jsx'
 
 export default function Navbar() {
   const { user, logout } = useAuth() || {}
