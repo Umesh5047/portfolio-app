c890d5585988d4893a4df06b599eb05ebeede2e2
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 15:21:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import Home from './pages/Home.jsx'
-import Portfolio from './pages/Portfolio.jsx'
-import Projects from './pages/Projects.jsx'
-import ProjectDetail from './pages/ProjectDetail.jsx'
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
-export default function App() {
+export default function App(){
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+    <div className="min-h-screen">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:id" element={<ProjectDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path='/' element={<Home/>} />
+          <Route path='/portfolio' element={<Portfolio/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/projects/:id' element={<ProjectDetail/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
         </Routes>
       </main>
     </div>
