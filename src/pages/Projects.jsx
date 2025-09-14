0e1aec9ccb555cf6e7218e28d0d1ad553aca37e1
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 12:33:15 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard'
-import { projects } from '../data/sampleProjects' // update path if needed
+import { projects } from '../data/sampleProjects'
 
 export default function Projects() {
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">Projects</h2>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projects?.map(p => (
-          <ProjectCard key={p.id} project={p} />
-        ))}
+        {projects.map(p => <ProjectCard key={p.id} project={p} />)}
       </div>
     </div>
   )
