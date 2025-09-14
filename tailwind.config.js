28e693a5cf06864d65c60dc98196a75629590650
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 11:41:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        accent: '#06b6d4',
+      }
+    }
+  },
   plugins: [],
-};
+}
