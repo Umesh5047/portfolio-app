69b5c218f4f8136ea60895ad60ec345d06a5e961
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 12:32:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard'
-import { projects } from '../data/sampleProjects' // make sure this path is correct
+import { projects } from '../data/sampleProjects'
 
 export default function Home() {
   return (
     <div className="space-y-6 p-4">
       <h1 className="text-3xl font-bold">Welcome</h1>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projects?.map(p => (
-          <ProjectCard key={p.id} project={p} />
-        ))}
+        {projects.map(p => <ProjectCard key={p.id} project={p} />)}
       </div>
     </div>
   )
