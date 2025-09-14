64ce7d28bc7933de02d406763d602a9d1e01910e
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-14 10:11:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import ProjectDetail from './pages/ProjectDetail'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import ProjectDetail from './pages/ProjectDetail';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Navbar />
-      <main className="flex-1 p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/projects/:id" element={<ProjectDetail />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
